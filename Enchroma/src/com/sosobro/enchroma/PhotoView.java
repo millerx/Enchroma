1daedc302d99bc53c0bcaa58f627ecba4d40bece
millerx <millerx@gmail.com>
2013-07-04 11:39:10 -0400

    Raw save/restore on PhotoView.

--- Enchroma/src/com/sosobro/enchroma/PhotoView.java
+++ Enchroma/src/com/sosobro/enchroma/PhotoView.java
@@ -10,6 +10,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.view.View;
@@ -34,6 +36,30 @@ public class PhotoView extends ImageView
 		_reqId = reqId;
 	}
 	
+	// TODO: Print what was saved and loaded
+	@Override
+	protected Parcelable onSaveInstanceState( ) {
+		Parcelable p = super.onSaveInstanceState();
+
+		SavedState ss = new SavedState( p );
+		ss.stateToSave = (_subjectFile == null) ? null : _subjectFile.toString();
+		
+		return ss;
+	}
+
+	@Override
+	protected void onRestoreInstanceState( Parcelable state ) {
+		if (!(state instanceof SavedState)) {
+			super.onRestoreInstanceState( state );
+			return;
+		}
+
+		SavedState ss = (SavedState) state;
+		super.onRestoreInstanceState( ss.getSuperState() );
+		
+		_subjectFile = (ss.stateToSave == null) ? null : new File( ss.stateToSave );
+	}
+
 	@Override
 	public void onClick( View v ) {
 		// TODO: Refactor into this class.
@@ -45,6 +71,38 @@ public class PhotoView extends ImageView
 	}
 
 	public void onActivityResult( int resultCode, Intent data ) {
+		System.out.printf( "onActivityResult subjectFile=%s\n",
+			(_subjectFile == null) ? "null" : _subjectFile.toString() );
 		// TODO: Move Main.onPictureTaken
+	}
+	
+	static class SavedState extends BaseSavedState {
+		String stateToSave;
+
+		SavedState( Parcelable p ) {
+			super( p );
+		}
+
+		private SavedState( Parcel in ) {
+			super( in );
+			stateToSave = in.readString();
+		}
+
+		@Override
+		public void writeToParcel( Parcel out, int flags ) {
+			super.writeToParcel( out, flags );
+			out.writeString( this.stateToSave );
+		}
+
+		// required field that makes Parcelables from a Parcel
+		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+			public SavedState createFromParcel( Parcel in ) {
+				return new SavedState( in );
+			}
+
+			public SavedState[] newArray( int size ) {
+				return new SavedState[size];
+			}
+		};
 	}
 }
