f9266bba7b9577a811f4f6a60c9e3c50f1579acf
millerx <millerx@gmail.com>
2013-07-04 12:24:49 -0400

    Cleaned up save/restore.

--- Enchroma/src/com/sosobro/enchroma/PhotoView.java
+++ Enchroma/src/com/sosobro/enchroma/PhotoView.java
@@ -1,15 +1,16 @@
 package com.sosobro.enchroma;
 
 // Next:
-// - Wire up save/restore instance.
+// - Create and serialize thumb.
 // - Continue moving code over.
+// - Pull out CameraPhotoView vs SelectPhotoView
 import java.io.File;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.provider.MediaStore;
@@ -22,9 +23,51 @@ public class PhotoView extends ImageView
 
 	private Activity _activity;
 	private int _reqId;
-	// TODO: Remove reference to subject.
-	private File _subjectFile;
-	private Bitmap _subjectThumb;
+	private SavedState _ss = new SavedState();
+
+	static class SavedState extends BaseSavedState {
+		public File photoFile;
+
+		public SavedState( ) {
+			super( new Bundle() );
+		}
+		
+		public SavedState( Parcelable p, SavedState ss ) {
+			super( p );
+			photoFile = ss.photoFile;
+		}
+		
+		private SavedState( Parcel in ) {
+			super( in );
+			
+			{ // photoFile
+				String s = in.readString();
+				photoFile = (s == "") ? null : new File( s ); 
+			}
+			
+			printState("Restored state:");
+		}
+
+		@Override
+		public void writeToParcel( Parcel out, int flags ) {
+			super.writeToParcel( out, flags );
+			out.writeString( (photoFile == null) ? "" : photoFile.toString() );
+			
+			printState("Saved state:");
+		}
+
+		private void printState( String header ) {
+			System.out.println( header );
+			System.out.printf( "photoFile=%s\n", photoFile==null ? "null" : photoFile.toString() );
+			//System.out.printf( "thumb=%d\n", thumb==null ? 0 : thumb.getByteCount() );
+		}
+		
+		// required field that makes Parcelables from a Parcel
+		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+			public SavedState createFromParcel( Parcel in ) { return new SavedState( in ); }
+			public SavedState[] newArray( int size ) { return new SavedState[size]; }
+		};
+	}
 	
 	public PhotoView( Context context, AttributeSet attrs ) {
 		super( context, attrs );
@@ -39,70 +82,28 @@ public class PhotoView extends ImageView
 	// TODO: Print what was saved and loaded
 	@Override
 	protected Parcelable onSaveInstanceState( ) {
-		Parcelable p = super.onSaveInstanceState();
-
-		SavedState ss = new SavedState( p );
-		ss.stateToSave = (_subjectFile == null) ? null : _subjectFile.toString();
-		
-		return ss;
+		return new SavedState( super.onSaveInstanceState(), _ss );
 	}
 
 	@Override
 	protected void onRestoreInstanceState( Parcelable state ) {
-		if (!(state instanceof SavedState)) {
-			super.onRestoreInstanceState( state );
-			return;
-		}
-
-		SavedState ss = (SavedState) state;
-		super.onRestoreInstanceState( ss.getSuperState() );
-		
-		_subjectFile = (ss.stateToSave == null) ? null : new File( ss.stateToSave );
+		_ss = (SavedState) state;
+		super.onRestoreInstanceState( _ss.getSuperState() );
 	}
 
 	@Override
 	public void onClick( View v ) {
 		// TODO: Refactor into this class.
-		_subjectFile = PhotoUtils.instance.createSubjectFilePath( _activity );
+		_ss.photoFile = PhotoUtils.instance.createSubjectFilePath( _activity );
 		
 		Intent i = new Intent( MediaStore.ACTION_IMAGE_CAPTURE );
-		i.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile( _subjectFile ) );
+		i.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile( _ss.photoFile ) );
 		_activity.startActivityForResult( i, _reqId );
 	}
 
 	public void onActivityResult( int resultCode, Intent data ) {
-		System.out.printf( "onActivityResult subjectFile=%s\n",
-			(_subjectFile == null) ? "null" : _subjectFile.toString() );
+		System.out.printf( "onActivityResult photoFile=%s\n",
+			(_ss.photoFile == null) ? "null" : _ss.photoFile.toString() ); 
 		// TODO: Move Main.onPictureTaken
-	}
-	
-	static class SavedState extends BaseSavedState {
-		String stateToSave;
-
-		SavedState( Parcelable p ) {
-			super( p );
-		}
-
-		private SavedState( Parcel in ) {
-			super( in );
-			stateToSave = in.readString();
-		}
-
-		@Override
-		public void writeToParcel( Parcel out, int flags ) {
-			super.writeToParcel( out, flags );
-			out.writeString( this.stateToSave );
-		}
-
-		// required field that makes Parcelables from a Parcel
-		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
-			public SavedState createFromParcel( Parcel in ) {
-				return new SavedState( in );
-			}
-
-			public SavedState[] newArray( int size ) {
-				return new SavedState[size];
-			}
-		};
 	}
 }
