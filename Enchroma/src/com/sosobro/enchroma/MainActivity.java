dc6a434f30b5a6e259026e0fa04f9f095f1b5423
millerx <millerx@gmail.com>
2013-07-05 11:38:28 -0400

    Strip old code out of main.

--- Enchroma/src/com/sosobro/enchroma/MainActivity.java
+++ Enchroma/src/com/sosobro/enchroma/MainActivity.java
@@ -1,16 +1,10 @@
 package com.sosobro.enchroma;
 
-import java.io.File;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageView;
 
 // TODO: Optimize layout
 //       http://stackoverflow.com/questions/5254562/is-there-a-simpler-better-way-to-put-a-border-outline-around-my-textview
@@ -19,13 +13,6 @@ public class MainActivity extends Activity {
 
 	public static final int REQ_CAPTURESUBJECTPHOTO = 1;
 	
-	public static final String BN_SUBJECTFILE = "subjectFile";
-	public static final String BN_SUBJECTTHUMB = "subjectThumb";
-	
-	private File _subjectFile;
-	private Bitmap _subjectThumb;
-	//private File _backgroundFile;
-
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
@@ -44,22 +31,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	@Override
+	/*@Override
 	protected void onRestoreInstanceState( Bundle state ) {
 		super.onRestoreInstanceState( state );
-
-		if (state.containsKey( BN_SUBJECTFILE ))
-			_subjectFile = new File( state.getString( BN_SUBJECTFILE ) );
-		_subjectThumb = (Bitmap) state.getParcelable( BN_SUBJECTTHUMB );
-		
-		printState( "restoreInstanceState" );
-	}
-
-	private void printState( String header ) {
-		//System.out.println( header );
-		//System.out.printf( "subjectFile=%s\n", _subjectFile==null ? "" : _subjectFile.toString() );
-		//System.out.printf( "subjectBitmap=%d\n", _subjectThumb==null ? 0 : _subjectThumb.getByteCount() );
-	}
+	}*/
 
 	@Override
 	public void onActivityResult( int requestCode, int resultCode, Intent data ) {
@@ -67,56 +42,21 @@ public class MainActivity extends Activity {
 
 		System.out.printf("onActivityResult req=%d res=%d\n", requestCode, resultCode);
 
-		// TODO: No need for resultCode check.
-		if (requestCode == REQ_CAPTURESUBJECTPHOTO && resultCode == RESULT_OK) {
+		if (requestCode == REQ_CAPTURESUBJECTPHOTO) {
 			PhotoView v = (PhotoView) findViewById( R.id.subjectImg );
 			v.onActivityResult( resultCode, data );
 		}
-		else if (requestCode == Common.REQ_SELECTBACKGROUND && resultCode == RESULT_OK) {
-			onBackgroundSelected();
-		}
 	}
 
-	@Override
+	/*@Override
 	protected void onResume( ) {
 		super.onResume();
-
-		if (_subjectThumb != null) {
-			ImageView iv = (ImageView) findViewById( R.id.subjectImg );
-			iv.setImageBitmap( _subjectThumb );
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState( Bundle state ) {
-		super.onSaveInstanceState( state );
-
-		printState( "saveInstanceState" );
-		
-		if (_subjectFile != null)
-			state.putString( BN_SUBJECTFILE, _subjectFile.toString() );
-		if (_subjectThumb != null)
-			state.putParcelable( BN_SUBJECTTHUMB, _subjectThumb );
-	}
-	
-	/*public void subjectImg_onClick( View v ) {
-		_subjectFile = PhotoUtils.instance.createSubjectFilePath( this );
-		
-		Intent i = new Intent( MediaStore.ACTION_IMAGE_CAPTURE );
-		i.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile( _subjectFile ) );
-		startActivityForResult( i, REQ_CAPTURESUBJECTPHOTO );
 	}*/
 
-	private void onPictureTaken( ) {
-		_subjectThumb = PhotoUtils.instance.createThumbnail( _subjectFile );
-	}
-	
-	public void backgroundImg_onClick( View v ) {
-		// TODO: Pick bg image
-	}
-
-	private void onBackgroundSelected( ) {
-	}
+	/*@Override
+	protected void onSaveInstanceState( Bundle state ) {
+		super.onSaveInstanceState( state );
+	}*/
 	
 	public void engageBtn_onClick( View v ) {
 		// TODO: Enage
--- EnchromaTest/src/com/sosobro/enchroma/test/MainActivityTest.java
+++ EnchromaTest/src/com/sosobro/enchroma/test/MainActivityTest.java
@@ -1,16 +1,9 @@
 package com.sosobro.enchroma.test;
 
-import java.io.File;
-
 import com.sosobro.enchroma.*;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
 import android.test.ActivityUnitTestCase;
-import android.widget.ImageView;
 
 public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 	
@@ -18,87 +11,16 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 		super( MainActivity.class );
 	}
 
-	private class TestPhotoUtils extends PhotoUtils {
-		
-		public static final String SubjectFilePath = "/storage/sdcard0/Pictures/Enchroma/subject.jpg";
-		
-		@Override
-		public File createSubjectFilePath( Activity a ) {
-			return new File( SubjectFilePath );
-		}
-
-		@Override
-		public Bitmap createThumbnail( Bitmap b ) {
-			return b;
-		}
-		
-		@Override
-		public Bitmap createThumbnail( File f ) {
-			return Bitmap.createBitmap( 8, 8, Bitmap.Config.RGB_565 );
-		}
-	}
-	
 	protected void setUp( ) throws Exception {
 		super.setUp();
-		
-		PhotoUtils.instance = new TestPhotoUtils();
 		
 		startActivity(
 			new Intent( getInstrumentation().getTargetContext(), MainActivity.class ),
 			null, null );
 	}
-
-	public void testSaveAndRestoreEmptyState( ) {
-		MainActivity a = getActivity();
-		
-		getInstrumentation().callActivityOnRestoreInstanceState( a, new Bundle() );
-		
-		// Nothing blew up?  Good.
-		
-		Bundle outState = new Bundle();
-		getInstrumentation().callActivityOnSaveInstanceState( a, outState );
-		
-		// Can we restore what was saved?
-		getInstrumentation().callActivityOnRestoreInstanceState( a, outState );
-	}
 	
-	public void testTakeSubjectPhoto( ) {
-		MainActivity a = getActivity();
-		
-		//a.subjectImg_onClick( null );
-
-		// Did we start the camera activity specifying the file path?
-		Intent i = getStartedActivityIntent();
-		assertEquals( MediaStore.ACTION_IMAGE_CAPTURE,
-			getStartedActivityIntent().getAction() );
-		assertEquals( MainActivity.REQ_CAPTURESUBJECTPHOTO, getStartedActivityRequest() );
-		assertEquals( "file:///storage/sdcard0/Pictures/Enchroma/subject.jpg",
-			i.getExtras().get( MediaStore.EXTRA_OUTPUT ).toString() );
-
-		// When the onClick method completes the activity is paused and the camera activity starts.
-		// We should have saved our current state.
-		Bundle outState = new Bundle();
-		getInstrumentation().callActivityOnSaveInstanceState( a, outState );
-		assertEquals( TestPhotoUtils.SubjectFilePath, outState.getString( "subjectFile" ) );
-		
-		// TODO: Test if path was created.
-	}
-
-	public void testCameraActivityReturns( ) {
-		MainActivity a = getActivity();
-		
-		// Restore state from when subjectImg was clicked.
-		Bundle inState = new Bundle();
-		inState.putString( MainActivity.BN_SUBJECTFILE, TestPhotoUtils.SubjectFilePath );
-		getInstrumentation().callActivityOnRestoreInstanceState( a, inState );
-
-		// Intent from camera activity with a file path is null.
-		a.onActivityResult( MainActivity.REQ_CAPTURESUBJECTPHOTO, Activity.RESULT_OK, null );
-		
-		getInstrumentation().callActivityOnResume( a );
-
-		// For our tests the thumb is an 8x8 bitmap.
-		ImageView iv = (ImageView) a.findViewById( com.sosobro.enchroma.R.id.subjectImg );
-		assertEquals( 8, iv.getDrawable().getIntrinsicWidth() );
+	public void testSmoke( )
+	{
+		assertNotNull( getActivity() );
 	}
 }
